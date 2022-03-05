f20e3217ae52d0661a832c7acafd66eba8d96c44
Soumobrata Manna <82120717+soumo99@users.noreply.github.com>
2022-03-06 00:06:21 +0530

    Add files via upload

---
+++ radius_circle.py
@@ -0,0 +1,7 @@
+rad = int(input("Enter the radius of the circle :"))
+
+if(rad > 0):
+    area = 3.14 * rad * rad
+    print("The area of the circle with radius ", rad , "is : ",area)
+else:
+    print("Please enter a valid digit for the area calculation ")
