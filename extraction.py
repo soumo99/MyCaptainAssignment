6c38bd6aa53546d0dabe55af0daa3c4770027583
Soumobrata Manna <82120717+soumo99@users.noreply.github.com>
2022-03-07 22:58:26 +0530

    Delete extraction.py

--- extraction.py
+++
@@ -1,7 +0,0 @@
-fname = input("Input the Filename: ")
-file_extension = fname.split(".")
-if (fname == "abc.py"):
-    print("The extension of the file is : Python")
-else:
-    print("check the file extension carefully !!")
-
