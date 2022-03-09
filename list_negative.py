d4636780c91718fc86099732af8d042438c2774f
Soumobrata Manna <82120717+soumo99@users.noreply.github.com>
2022-03-10 00:59:08 +0530

    Add files via upload

---
+++ list_negative.py
@@ -0,0 +1,25 @@
+#First list
+list1 = [12, -7, 5, 64, -14]
+print("The first list is : ", list1)
+
+for n in list1:
+    if n >= 0:
+
+        print(n,end = " ")
+
+#Second list
+list2 = [12, 14, -95, 3]
+arr = []
+print("\nThe second list is :",list2)
+
+for y in list2:
+    if y >= 0:
+        arr.append(y)
+print(arr)
+
+
+
+
+
+
+
